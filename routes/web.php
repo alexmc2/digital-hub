dffe337b9b52855332743207233ce0b56b0fe68a
AlexMc2 <alex_mcgarry@hotmail.co.uk>
2024-03-07 13:51:09 +0000

    set up user login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExampleController;
 
 /*
@@ -9,11 +10,13 @@ use App\Http\Controllers\ExampleController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', [ExampleController::class, 'homepage']); // This is the home page
+Route::get('/', [UserController::class, "showCorrectHomepage"]);
+Route::get('/about', [ExampleController::class, "aboutPage"]);
 
-Route::get('/about', [ExampleController::class, 'about']); // This is the about page
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
